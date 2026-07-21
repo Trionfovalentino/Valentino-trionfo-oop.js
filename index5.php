84064b05afa52e20202d6cbe1a476a257d96efb8
Trionfovalentino <trionfovalentino26@gmail.com>
2026-07-21 12:30:59 +0200

    codice aggiornato

--- index5.php
+++ index5.php
@@ -1,57 +1,64 @@
 <?php
 
-require_once 'class.php';
+// Classe base/astratta per le categorie
+abstract class Category {
+    public $name;
 
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    abstract public function getMyCategory();
+}
+
+// Subclassi specifiche
+class Sport extends Category {
+    public function __construct() {
+        parent::__construct("Sport");
+    }
+
+    public function getMyCategory() {
+        echo "CATEGORIA: " . $this->name . "\n";
+    }
+}
+
+class Gossip extends Category {
+    public function __construct() {
+        parent::__construct("Gossip");
+    }
+
+    public function getMyCategory() {
+        echo "CATEGORIA: " . $this->name . "\n";
+    }
+}
+
+class Attualita extends Category {
+    public function __construct() {
+        parent::__construct("Attualità");
+    }
+
+    public function getMyCategory() {
+        echo "CATEGORIA: " . $this->name . "\n";
+    }
+}
+
+// Classe Post
 class Post {
-
     public $title;
     public $category; 
     public $tag;
 
-  public function __construct($title, Category $category, $tag) {
+    public function __construct($title, Category $category, $tag) {
         $this->title = $title;
         $this->category = $category;
         $this->tag = $tag;
     }
 
-
     public function displayPost() {
         echo "==================================================\n";
         echo "TITOLO: " . $this->title . "\n";
         echo "TAG: " . $this->tag . "\n";
-        
-      $this->category->getMyCategory(); 
+        $this->category->getMyCategory(); 
         echo "==================================================\n\n";
     }
 }
-
-
-$catSport = new Sport();
-$catGossip = new Gossip();
-$catAttualita = new Attualita();
-
-$post1 = new Post(
-    "Il Milan vince il derby all'ultimo minuto!", 
-    $catSport, 
-    "#calcio #sport #milan"
-);
-
-$post2 = new Post(
-    "Nuovo scoop estivo: beccata la famosa coppia a Capri", 
-    $catGossip, 
-    "#gossip #estate #paparazzi"
-);
-
-$post3 = new Post(
-    "Nuove riforme economiche approvate in parlamento", 
-    $catAttualita, 
-    "#politica #attualita #news"
-);
-
-
-echo "--- ELENCO ARTICOLI PUBBLICATI ---\n\n";
-$post1->displayPost();
-$post2->displayPost();
-$post3->displayPost();
-
-?>
